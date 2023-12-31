5bbbe61fe4f2219ca80a0d69c0ae624bbc52fe9d
JEvgeniy01 <evgeniy.turov@mail.ru>
2023-12-31 16:30:11 +0700

    feat: has and find

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,30 +1,80 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
+// class Node {
+//   constructor(data) {
+//     this.data = data;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
 class BinarySearchTree {
+  constructor() {
+    this.rooty = null;
+  }
 
   root() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
+    return this.rooty ? this.rooty : null;
     // remove line with error and write your code here
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  add(data) {
+    // throw new NotImplementedError('Not implemented');
+   this.rooty = addWithin(this.rooty, data);
+   function addWithin(node, value) {
+    //
+    if (!node) {
+      return new Node(value)
+    }
+    if (node.data === value) {
+      return node
+    }
+    if (value < node.data) {
+      node.left = addWithin(node.left, value)
+    } else { 
+      node.right = addWithin(node.right, value)
+    }
+    return node
+   }
   }
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  has(data) {
+    // throw new NotImplementedError('Not implemented');
+    return searchValue(this.rooty, data);
+    function searchValue(node, value) {
+      if (!node) {
+        return false;
+      }
+      if (node.data === value) {
+        return true;
+      }
+      if (value < node.data) {
+        return searchValue(node.left, value)
+      } else {
+        return searchValue(node.right, value)
+      }
+    }
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  find(data) {
+    // throw new NotImplementedError('Not implemented');
+    return findValue(this.rooty, data);
+    function findValue(node, value) {
+      if (!node) {
+        return null;
+      }
+      if (node.data === value) {
+        return node;
+      }
+     return value < node.data ? findValue(node.left, value) : findValue(node.right, value);
+    }
     // remove line with error and write your code here
   }
 
@@ -35,6 +85,7 @@ class BinarySearchTree {
 
   min() {
     throw new NotImplementedError('Not implemented');
+
     // remove line with error and write your code here
   }
 
